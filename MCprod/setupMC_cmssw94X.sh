778f00ef373cda892824d48141b713a29b3ac866
Jing <boundino@gmail.com>
2018-12-28 02:39:25 -0500

    add MC production config

---
+++ MCprod/setupMC_cmssw94X.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cmsrel CMSSW_9_4_12
+cd CMSSW_9_4_12/src/
+cmsenv
+git cms-addpkg GeneratorInterface/ExternalDecays
+git clone https://github.com/boundino/HFAnaGenFrags.git
+scram b -j4
+
+ln -s HFAnaGenFrags/Run2018PbPb502 .
+
+# Set up grid >>>
+echo $0
+which grid-proxy-info
+echo $SCRAM_ARCH
+voms-proxy-init --voms cms --valid 168:00
+voms-proxy-info --all
+# <<<
+
