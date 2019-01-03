c6ea3f4339e0d6fb24c635c52b3aeae3c5fe93d7
Jing <boundino@gmail.com>
2019-01-03 22:26:40 +0100

    update setupMC

--- MCprod/setupMC_cmssw103X.sh
+++ MCprod/setupMC_cmssw103X.sh
@@ -3,10 +3,18 @@
 cmsrel CMSSW_10_3_2
 cd CMSSW_10_3_2/src/
 cmsenv
+
 git cms-addpkg GeneratorInterface/ExternalDecays
 git clone https://github.com/boundino/HFAnaGenFrags.git
-
 ln -s HFAnaGenFrags/Run2018PbPb502 .
+
+git clone https://github.com/boundino/BmesonRun2018.git
+ln -s BmesonRun2018/MCprod/GenericAnalyzer .
+cp BmesonRun2018/MCprod/GenericAnalyzer/test/demoanalyzer_cfg.py .
+
+cp BmesonRun2018/MCprod/run_mc_gensim_cmssw94X.sh .
+ln -s BmesonRun2018/MCprod/utility.shinc .
+
 scram b -j4
 
 # Set up grid >>>
--- MCprod/setupMC_cmssw94X.sh
+++ MCprod/setupMC_cmssw94X.sh
@@ -5,8 +5,15 @@ cd CMSSW_9_4_12/src/
 cmsenv
 git cms-addpkg GeneratorInterface/ExternalDecays
 git clone https://github.com/boundino/HFAnaGenFrags.git
-
 ln -s HFAnaGenFrags/Run2018PbPb502 .
+
+git clone https://github.com/boundino/BmesonRun2018.git
+ln -s BmesonRun2018/MCprod/GenericAnalyzer .
+cp BmesonRun2018/MCprod/GenericAnalyzer/test/demoanalyzer_cfg.py .
+
+cp BmesonRun2018/MCprod/run_mc_gensim_cmssw94X.sh .
+ln -s BmesonRun2018/MCprod/utility.shinc .
+
 scram b -j4
 
 # Set up grid >>>
